9132ab6e9bc9c902026de9216669db3988328fb6
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-08 15:38:20 -0300

    Estilizado a página dos produtos

--- src/Components/Produtos.jsx
+++ src/Components/Produtos.jsx
@@ -14,14 +14,12 @@ const Produtos = () => {
   if (produtos === null) return null;
   return (
     <section className={styles.produtos + ' animeLeft'}>
-      <div>
-        {produtos.map((produto) => (
-          <Link to={`produto/${produto.id}`} key={produto.id}>
-            <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
-            <h1 className={styles.nome}>{produto.nome}</h1>
-          </Link>
-        ))}
-      </div>
+      {produtos.map((produto) => (
+        <Link to={`produto/${produto.id}`} key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+          <h1 className={styles.nome}>{produto.nome}</h1>
+        </Link>
+      ))}
     </section>
   );
 };
