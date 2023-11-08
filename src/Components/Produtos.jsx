1040b0dabf519f427fbbe90494c2df348fb0c34b
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-08 15:17:29 -0300

    Criado o estado inicial da página e o fetch de puxar a api, transforma-la em .json e esse .json passar para o estado do produto

--- src/Components/Produtos.jsx
+++ src/Components/Produtos.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import styles from './Produtos.module.css';
 
 const Produtos = () => {
+  const [produtos, setProdutos] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+      .then((r) => r.json())
+      .then((json) => setProdutos(json));
+  });
+
   return (
     <div className={styles.produtos}>
       <h1>Produtos</h1>
