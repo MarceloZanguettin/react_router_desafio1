72a60813991b9d13baec2c9a2ff899c112fdd737
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-08 14:26:07 -0300

    Adicionado o caminho para pagina inicial que é a pagina de Produtos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Produtos />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
