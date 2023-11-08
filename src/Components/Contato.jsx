59fbbe6dd4a2c8389bb8e8c13eb85e7585aae162
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-08 15:06:20 -0300

    Criado o .css do Contato

--- src/Components/Contato.jsx
+++ src/Components/Contato.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './Contato.module.css';
+import foto from '../img/contato.jpg';
 
 const Contato = () => {
   return (
-    <div>
-      <h1>Contato</h1>
-    </div>
+    <section className={styles.contato}>
+      <img src={foto} alt="Máquina de escrever" />
+      <div>
+        <h1>Entre em contato.</h1>
+        <ul className={styles.dados}>
+          <li>contato@email.com</li>
+          <li>(99) 9999-9999</li>
+          <li>Rua Qualquer, 999</li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
